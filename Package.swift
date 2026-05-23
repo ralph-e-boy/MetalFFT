c515e5cf325fb00725c4cf91e5b29f2e6c886346
ralph <cobble-immured-69@icloud.com>
2026-05-23 14:04:27 -0400

    add llmref tool

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         .executable(name: "DNASpectralDemo", targets: ["DNASpectralDemo"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        .package(url: "https://github.com/ralph-e-boy/swift-llmref", from: "0.0.1")
     ],
     targets: [
         .target(
