b3b4e6f3102a94977b7a24ded7616ea91f421a15
ralph <cobble-immured-69@icloud.com>
2026-06-02 09:29:47 -0400

    update llmref vers

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/ralph-e-boy/swift-llmref", from: "0.0.1")
+        .package(url: "https://github.com/ralph-e-boy/swift-llmref", from: "0.0.3")
     ],
     targets: [
         .target(
